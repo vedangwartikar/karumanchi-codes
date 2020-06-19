01573adbb2098a56ed95726cad5ad8a0347e47b1
Vedang Wartikar <vedangwartikar17@gmail.com>
2020-06-19 13:24:52 +0530

    direction for bfs

--- 2. Recursion and Backtracking/findMaxBlock.py
+++ 2. Recursion and Backtracking/findMaxBlock.py
@@ -1,4 +1,47 @@
-"""arr = [[1,0,0,1],[1,1,0,1],[0,0,1,1]]
+def getval(A, i, j, L, H):
+    if i < 0 or i >= L or j < 0 or j >= H:
+        return 0
+    else:
+        return A[i][j]
+
+def findMaxBlock(A, r, c, L, H, size):
+    global maxsize
+    global cntarr
+    if r >= L or c >= H:
+        return
+    cntarr[r][c] = 1
+    size += 1
+    if size > maxsize:
+        maxsize = size
+    direction = [[-1,0],[-1,-1],[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1]]
+    for i in range(0, 7):
+        newi = r + direction[i][0]
+        newj = c + direction[i][1]
+        val = getval(A, newi, newj, L, H)
+        if val > 0 and cntarr[newi][newj] == 0:
+            findMaxBlock(A, newi, newj, L, H, size)
+    cntarr[r][c] = 0
+
+def getMaxOnes(A, rmax, colmax):
+    global maxsize
+    global size
+    global cntarr
+    for i in range(0, rmax):
+        for j in range(0, colmax):
+            if A[i][j] == 1:
+                findMaxBlock(A, i, j, rmax, colmax, 0)
+    return maxsize
+
+zarr = [[1,1,0,0,0],[0,1,1,0,1],[0,0,0,1,1],[1,0,0,1,1],[0,1,0,1,1]]
+rmax = 5
+colmax = 5
+maxsize = 0
+size = 0
+cntarr = rmax*[colmax*[0]]
+print("Number of maximum ones are: ", end = "")
+print(getMaxOnes(zarr, rmax, colmax))
+"""
+arr = [[1,0,0,1],[1,1,0,1],[0,0,1,1]]
 cntarr = []
 for i in arr:
     for j in i:
@@ -27,4 +70,5 @@ for i in range(m):
         else:
             cnt = 0
 #print(cntarr)
-print(max(cntarr))"""
+print(max(cntarr))
+"""
